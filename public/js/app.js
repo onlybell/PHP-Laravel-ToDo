c065c85d6515a43a19d035c714257cd47e909b74
onlybell <torok0521@gmail.com>
2020-12-31 16:47:33 +1300

    Authentication

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/XAMPP/xamppfiles/docs/PHP-Laravel-Login/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/docs/PHP-Laravel-Login/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/onlybell/Desktop/Projects/02PHP/PHP-Laravel-ToDo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/onlybell/Desktop/Projects/02PHP/PHP-Laravel-ToDo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/layouts/loginPage.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>To-Do</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+
+               <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    </li>
+                </ul>
+                
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
